2c33fef5472480e9b5df725f54ab1302b5563f9c
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2024-12-07 12:37:49 +0100

    [Day 7] Add solution to part 2
    After several attempts to figure out with caching the results, only caching the operators.
    Part 2 runs on roughly 40 secs, with caching it was closer to 60 so yeah
    Caching operators leads to a 1-2 secs difference

--- sol_snake/day_7.py
+++ sol_snake/day_7.py
@@ -1,10 +1,10 @@
 from typing import TextIO
 import itertools
-
+from collections import deque
+import time
 
 # test_value, operator_places, numbers
 type Equation = tuple[int, int, list[int]]
-
 
 def do_operation(op: str, first: int, second: int) -> int:
     match op:
@@ -12,25 +12,26 @@ def do_operation(op: str, first: int, second: int) -> int:
             return first + second
         case "*":
             return first * second
+        case "||":
+            return int(f"{first}{second}")
         case _:
             raise ValueError("Wrong operator!")
 
 def can_be_solved(x: int, operators_combinations: list[tuple[str, ...]], numbers: list[int]) -> bool:
-    solutions = []
     for ops in operators_combinations:
-        numbers_left = numbers[:]
-        ops_left = list(ops)
+        numbers_left = deque(numbers)
+        ops_left = deque(ops)
         while len(numbers_left) > 1 and len(ops_left) > 0:
-            first = numbers_left.pop(0)
-            second = numbers_left.pop(0)
-            operator = ops_left.pop(0)
+            first = numbers_left.popleft()
+            second = numbers_left.popleft()
+            operator = ops_left.popleft()
+
+            # cachng the operation result would add enough overhead to make it 10-20 secs slower
             result = do_operation(operator, first, second)
-            numbers_left.insert(0, result)
+            numbers_left.appendleft(result)
         if numbers_left[0] == x:
-            solutions.append(operators_combinations)
-    return len(solutions) > 0
-
-
+            return True
+    return False
 
 def parse_equations(content: TextIO) -> list[Equation]:
     result = []
@@ -48,18 +49,44 @@ def day_7(content: TextIO, example: bool) -> tuple[int, int]:
 
     equations = parse_equations(content)
 
-    operators = ["*", "+"]
+    #start = time.time()
+    #print("Start", 0, start)
+    operators_part_1 = ["*", "+"]
+    results_part_1 = []
 
-    result_part_1 = []
+    to_check_part_2 = []
+
     for x, places, numbers in equations:
-        possible_solutions = list(itertools.product(operators, repeat=places))
+        possible_solutions = list(itertools.product(operators_part_1, repeat=places))
+
         if can_be_solved(x, possible_solutions, numbers):
-            result_part_1.append(x)
+            results_part_1.append(x)
+        else:
+            to_check_part_2.append((x, places, numbers))
 
+    result_part_1 = sum(results_part_1)
+    #part_1_time = time.time()
+    #print("Part 1", part_1_time - start, part_1_time)
+    #print("Part 1: ", result_part_1)
 
+    #print("PART 2")
+    operators_part_2 = ["*", "+", "||"]
+    result_part_2 = []
 
-    print("PART 2")
+    # caching the operations list actually makes it faster
+    possible_solutions_cache_2 : dict[int, list[tuple[str,...]]] = dict()
+    for x, places, numbers in to_check_part_2:
+        possible_solutions = possible_solutions_cache_2.get(places, None)
+        if possible_solutions is None:
+            # We need to cast the result of iter tools to a list to consume the generator (?)
+            # For whatever reason, if we try to cast it after, or use it as was, it'll just become None / empty list
+            possible_solutions = list(itertools.product(operators_part_2, repeat=places))
+            possible_solutions_cache_2[places] = possible_solutions
 
-    result_part_2 = 0
+        if can_be_solved(x, possible_solutions, numbers):
+            result_part_2.append(x)
 
-    return (sum(result_part_1), result_part_2)
+    #part_2_time = time.time()
+    #print("Part 2", part_2_time - start, part_2_time)
+
+    return (result_part_1, result_part_1 + sum(result_part_2))
