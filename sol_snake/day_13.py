4645e4a4547aa0b7a96069414e12017e9520d9d7
Miguel Fradinho Alves <miguel.fradinho@ua.pt>
2024-12-13 15:03:26 +0100

    [Day 13] Add solution to part 2
    turns out rounding was right, but needed to tweak the threshold for it
    
    Cba'ed to use determinants tbh

--- sol_snake/day_13.py
+++ sol_snake/day_13.py
@@ -15,18 +15,13 @@ def parse_machine(lines: list[str])-> tuple[nptypes.NDArray, nptypes.NDArray]:
     a_x, a_y = utils.parse_ints(button_a)
     b_x, b_y = utils.parse_ints(button_b)
 
-    equations_vector = np.array([[a_x, b_x], [a_y, b_y]], dtype=np.int32)
-    solution_vector = np.array([int(i) for i in re.findall(parse_ints, prize)], dtype=np.int32)
+    equations_vector = np.array([[a_x, b_x], [a_y, b_y]], dtype=np.int64)
+    solution_vector = np.array([int(i) for i in re.findall(parse_ints, prize)], dtype=np.int64)
 
     return (equations_vector,solution_vector)
 
-def solution_is_valid(num: float):
-    if num > 100:
-        return False
-    elif not math.isclose(num, round(num)):
-        return False
-    return True
-
+def is_close_to_integer(num: float):
+    return math.isclose(num, round(num), rel_tol=0, abs_tol=1e-4)
 
 def day_13(content: TextIO, example: bool) -> tuple[int, int]:
 
@@ -45,22 +40,22 @@ def day_13(content: TextIO, example: bool) -> tuple[int, int]:
     equations.append(equation)
 
     results_part_1 = 0
+    results_part_2 = 0
+
+    part_2_factor = 10000000000000
     for i in equations:
         equation, solutions = i
-        a_amount, b_amount = np.linalg.solve(equation, solutions)
+        a_amount_part_1, b_amount_part_1 = np.linalg.solve(equation, solutions)
 
-        if not solution_is_valid(a_amount):
-            #print(equation, solutions, a_amount, b_amount)
-            #print("Oofie, A isn't valid, skipping")
-            continue
-        elif not solution_is_valid(b_amount):
-            #print(equation, solutions, a_amount, b_amount)
-            #print("Oofie, B isn't valid, skipping")
-            continue
-        else:
-            #print(equation, solutions, a_amount, b_amount)
-            #print(a_amount, b_amount)
-            results_part_1 += (a_amount * 3 + b_amount * 1)
+        # part 1:
+        if is_close_to_integer(a_amount_part_1) and is_close_to_integer(b_amount_part_1):
+            if a_amount_part_1 <= 100 and b_amount_part_1 <= 100:
+                results_part_1 += (a_amount_part_1 * 3 + b_amount_part_1 * 1)
 
-    results_part_2 = 0
-    return (int(results_part_1), results_part_2)
+        # part 2
+        solutions_part_2 = solutions + part_2_factor
+        a_amount_part_2, b_amount_part_2 = np.linalg.solve(equation, solutions_part_2)
+        if is_close_to_integer(a_amount_part_2) and is_close_to_integer(b_amount_part_2):
+            results_part_2 += (a_amount_part_2 * 3 + b_amount_part_2 * 1)
+
+    return (int(results_part_1), int(results_part_2))
